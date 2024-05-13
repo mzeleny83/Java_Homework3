2536c0b2ea77d0db5c2cbea85d9c7f542be657a9
Miroslav Zeleny <m.zeleny@volny.cz>
2024-05-13 11:30:47 +0200

    úpravy

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
+public class Guest {
+    private String Name;
+    private String Surname;
+    private LocalDate Birthdate;
 }
+
+public class Room {
+    private int number;
+    private int beds;
+    private LocalDate birthdate;
+    private boolean hasBalcony;
+    private boolean isSeaSide;
+    private BigDecimal price;
+}
+
+public class Booking{
+    private list <String> guests;
+    private LocalDate startDate;
+    private LocalDate endaDate;
+    private boolean isBusinessTrip;
